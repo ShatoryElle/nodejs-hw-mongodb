4276acdecc7a6e029d121343f92c4fd05e916d19
Viktoria <Shatory@gmail.com>
2025-05-09 20:16:54 +0300

    Update importContacts.js

--- importContacts.js
+++ importContacts.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
-import Contact from './src/db/models/contact.js'; // Вказати правильний шлях
+import Contact from './src/db/models/contact.js';
 const contactsData = require('./contacts.json');
 
 
 
 const importContacts = async () => {
   try {
-    // Підключення до MongoDB
+
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -14,7 +14,7 @@ const importContacts = async () => {
 
     console.log('MongoDB connected!');
 
-    // Імпорт контактів
+ 
     await Contact.insertMany(contactsData);
 
     console.log('Contacts have been imported successfully!');
